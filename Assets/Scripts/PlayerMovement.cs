6fb9736acddfd2fb9a7e2e3a1a1ead98bab3f46c
Chris Yuan Zhong <chrisyuanzhong@outlook.com>
2023-07-07 17:41:12 -0600

    Added Background

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D playerRb;
     public float speed = 5f;
     public float input;
+    public SpriteRenderer playerSprite;
 
     // Start is called before the first frame update
     void Start()
